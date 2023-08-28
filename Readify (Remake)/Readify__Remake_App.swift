1ca2fbeca82cf1f445bb337603dcb5f9f53e19a6
hafizhMo <hafizh.hizb@gmail.com>
2023-08-28 22:50:59 +0700

    Initial Commit

---
+++ Readify (Remake)/Readify__Remake_App.swift
@@ -0,0 +1,17 @@
+//
+//  Readify__Remake_App.swift
+//  Readify (Remake)
+//
+//  Created by Hafizh Mo on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Readify__Remake_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
