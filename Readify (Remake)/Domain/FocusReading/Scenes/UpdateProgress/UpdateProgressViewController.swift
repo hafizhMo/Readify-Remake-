aa88d99db455f465176caa19cc5c1afc11a63a8a
hafizhMo <hafizh.hizb@gmail.com>
2023-09-02 23:09:11 +0700

    create UI UpdateProgress

---
+++ Readify (Remake)/Domain/FocusReading/Scenes/UpdateProgress/UpdateProgressViewController.swift
@@ -0,0 +1,28 @@
+//
+//  UpdateProgressViewController.swift
+//  Readify (Remake)
+//
+//  Created by Hafizh Mo on 02/09/23.
+//
+
+import UIKit
+
+class UpdateProgressViewController: UIViewController {
+  
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    setup()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    setup()
+  }
+  
+  private func setup() {}
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
