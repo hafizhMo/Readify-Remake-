32560c928832ed9a78f1b22cabe4e78115b67618
hafizhMo <hafizh.hizb@gmail.com>
2023-09-02 22:42:58 +0700

    create UI CompleteView

---
+++ Readify (Remake)/Domain/FocusReading/Scenes/Complete/CompleteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UpdateGoalViewController.swift
+//  Readify (Remake)
+//
+//  Created by Hafizh Mo on 02/09/23.
+//
+
+import UIKit
+
+class CompleteViewController: UIViewController {
+  
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    setup()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    setup()
+  }
+  
+  private func setup() {}
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
