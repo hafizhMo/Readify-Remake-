59f904b729c9e884c843d3b65713e51435abeb22
hafizhMo <hafizh.hizb@gmail.com>
2023-09-03 23:04:08 +0700

    prepare UserDefaults for Goal

---
+++ Readify (Remake)/Domain/FocusReading/Shared/GoalUserDefaultWorker.swift
@@ -0,0 +1,30 @@
+//
+//  GoalUserDefaultWorker.swift
+//  Readify (Remake)
+//
+//  Created by Hafizh Mo on 03/09/23.
+//
+
+import Foundation
+
+class GoalUserDefaultWorker {
+  func getTitle() -> String? {
+    let titleBook = SharedPreference.getString(key: .bookTitle)
+    return titleBook.isEmpty ? nil : titleBook
+  }
+  
+  func getTotal() -> Int? {
+    let totalPages = SharedPreference.getInt(key: .totalPages)
+    return totalPages == 0 ? nil : totalPages
+  }
+  
+  func getProgress() -> Int? {
+    let progress = SharedPreference.getInt(key: .progress)
+    return progress == 0 ? nil : progress
+  }
+  
+  func getTimer() -> Double? {
+    let timer = SharedPreference.getDouble(key: .timer)
+    return timer == 0 ? nil : timer
+  }
+}
---
+++ Readify (Remake)/Utils/SharedPreference.swift
@@ -0,0 +1,66 @@
+//
+//  UserDefaultWorker.swift
+//  Readify (Remake)
+//
+//  Created by Hafizh Mo on 03/09/23.
+//
+
+import Foundation
+
+enum PrefKeys: String {
+  case bookTitle
+  case totalPages
+  case progress
+  case timer
+  
+}
+
+struct SharedPreference {
+  static let pref = UserDefaults.standard
+  
+  static func commit() {
+    pref.synchronize()
+  }
+  
+  static func saveBool(key: PrefKeys, value: Bool) {
+    pref.set(value, forKey: key.rawValue)
+    commit()
+  }
+  
+  static func saveInt(key: PrefKeys, value: Int) {
+    pref.set(value, forKey: key.rawValue)
+    commit()
+  }
+  
+  static func saveDouble(key: PrefKeys, value: Double) {
+    pref.set(value, forKey: key.rawValue)
+    commit()
+  }
+  
+  static func saveString(key: PrefKeys, value: String) {
+    pref.set(value, forKey: key.rawValue)
+    commit()
+  }
+  
+  static func getString(key: PrefKeys) -> String {
+    if let string = pref.string(forKey: key.rawValue) {
+      return string
+    }
+    return ""
+  }
+  
+  static func getBool(key: PrefKeys) -> Bool {
+    if pref.bool(forKey: key.rawValue) {
+      return pref.bool(forKey: key.rawValue)
+    }
+    return false
+  }
+  
+  static func getInt(key: PrefKeys) -> Int {
+    return pref.integer(forKey: key.rawValue)
+  }
+  
+  static func getDouble(key: PrefKeys) -> Double {
+    return pref.double(forKey: key.rawValue)
+  }
+}
