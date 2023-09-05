d282a30525ac36d9791a245e1c669dbe9346c816
hafizhMo <hafizh.hizb@gmail.com>
2023-09-05 17:56:55 +0700

    integration feature create & edit Goal

--- Readify (Remake)/Domain/FocusReading/Scenes/CreateGoal/CreateGoalViewController.swift
+++ Readify (Remake)/Domain/FocusReading/Scenes/CreateGoal/CreateGoalViewController.swift
@@ -7,7 +7,14 @@
 
 import UIKit
 
-class CreateGoalViewController: UIViewController {
+protocol CreateGoalDisplayLogic: NSObject {
+  func displayCreateForm()
+  func displayEditForm(viewModel: CreateGoal.PrepareGoal.ViewModel)
+  func displaySavedGoalCallback(viewModel: CreateGoal.SaveGoal.ViewModel)
+}
+
+class CreateGoalViewController: UIViewController, CreateGoalDisplayLogic {
+  var interactor: CreateGoalBusinessLogic?
   
   override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -19,10 +26,70 @@ class CreateGoalViewController: UIViewController {
     setup()
   }
   
-  private func setup() {}
+  private func setup() {
+    let viewController = self
+    let interactor = CreateGoalInteractor()
+    let presenter = CreateGoalPresenter()
+    viewController.interactor = interactor
+    interactor.presenter = presenter
+    presenter.viewController = viewController
+  }
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    prepareForm()
+  }
+  
+  @IBOutlet weak var titleNavigation: UINavigationItem!
+  
+  @IBOutlet weak var errorLabel: UILabel!
+  @IBOutlet weak var bookTitleTextField: UITextField!
+  @IBOutlet weak var totalPagesTextField: UITextField!
+  @IBOutlet weak var timerDurationPicker: UIDatePicker!
+  
+  @IBOutlet weak var createButton: UIButton!
+  @IBOutlet weak var updateButton: UIButton!
+  @IBOutlet weak var deleteButton: UIButton!
+  
+  @IBAction func didCreateButtonTapped(_ sender: Any) {
+    let request = CreateGoal.SaveGoal.Request(title: bookTitleTextField.text, total: totalPagesTextField.text, timer: timerDurationPicker.countDownDuration)
+    interactor?.saveGoal(request: request)
+  }
+  
+  @IBAction func didUpdateButtonTapped(_ sender: Any) {
+    let request = CreateGoal.SaveGoal.Request(title: bookTitleTextField.text, total: totalPagesTextField.text, timer: timerDurationPicker.countDownDuration)
+    interactor?.saveGoal(request: request)
+  }
+  
+  @IBAction func didDeleteButtonTapped(_ sender: Any) {
+  }
+  
+  func prepareForm() {
+    interactor?.prepareForm()
+  }
+  
+  func displayCreateForm() {
+    titleNavigation.title = "Create Goal"
+    updateButton.isHidden = true
+    deleteButton.isHidden = true
+  }
+  
+  func displayEditForm(viewModel: CreateGoal.PrepareGoal.ViewModel) {
+    titleNavigation.title = "Edit Goal"
+    createButton.isHidden = true
+    
+    bookTitleTextField.text = viewModel.title
+    totalPagesTextField.text = viewModel.total
+    timerDurationPicker.countDownDuration = viewModel.timer
+  }
+  
+  func displaySavedGoalCallback(viewModel: CreateGoal.SaveGoal.ViewModel) {
+    errorLabel.text = viewModel.message
+    errorLabel.isHidden = viewModel.isSuccess
+    
+    if viewModel.isSuccess {
+      // dismiss()
+    }
   }
 }
